cc54f16502fbfb39641322683a9ff508c3ad171b
Alumno <JavierCases>
2018-12-11 11:14:27 +0100

    entre la mitad del examen

--- common/models/reserva.js
+++ common/models/reserva.js
@@ -2,4 +2,13 @@
 
 module.exports = function(Reserva) {
 
+	Reserva.beforeRemote('create', function(context, user, next) {
+    //context.args.data.fecha = Date.now();
+    context.args.data.usuarioId = context.req.accessToken.userId;
+    if(context.args.data.desde>context.args.data.hasta){
+    	console.info("esto es un ERROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOR");
+    }
+    next();
+  });
+
 };
--- server/boot/automigrateDB.js
+++ server/boot/automigrateDB.js
@@ -4,6 +4,21 @@ module.exports = function(app) {
   if (process.env.AUTOMIGRATE) {
     app.dataSources.mysqlDS.automigrate(null, function(er) {
       if (er) throw er;
+      app.models.Usuario.create([{
+      nombre: 'Franciaso Javier',
+      apellidos: 'Cases Lázaroooo',
+      email:'javier@gmail.com',
+      password:'alumno'
+    },
+    {
+      nombre: 'prueba',
+      apellidos: 'prueba',
+      email:'prueba@gmail.com',
+      password:'alumno'
+    }], function(err, Usuario) {
+      if (err) throw err;
+      console.log('Modelos creados con esta informacion: \n', Usuario);
+    });
       console.log('Loopback tables created in ', app.dataSources.mysqlDS.adapter.name);
       app.loadFixtures()
         .then(function() {
